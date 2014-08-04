ddb50c6bb13de4cce1aded1cec7537aea63c741f
nakaji <nakaji126@gmail.com>
2014-08-05 02:29:27 +0900

    initial commit

---
+++ WebAPI/Analytics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Google.Apis.Analytics.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+
+namespace WebAPI
+{
+    public class Analytics
+    {
+        public async static Task<int> GetPvAsync()
+        {
+            // Azure Web サイトで動かす場合には WEBSITE_LOAD_USER_PROFILE = 1 必須
+            var analyticsKeyFile = ConfigurationManager.AppSettings["analyticsKeyFile"];
+            var certificate = new X509Certificate2(analyticsKeyFile, "notasecret", X509KeyStorageFlags.Exportable);
+
+            // Scopes は指定しないとエラーになる
+            var analyticsCredentialId = ConfigurationManager.AppSettings["analyticsCredentialId"];
+            var credential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer(analyticsCredentialId)
+            {
+                Scopes = new[] { AnalyticsService.Scope.Analytics, AnalyticsService.Scope.AnalyticsReadonly }
+            }.FromCertificate(certificate));
+
+            // HttpClientInitializer に credential 入れるのは違和感あるけど正しいらしい
+            var service = new AnalyticsService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "TweetPV",
+            });
+
+            // Azure は UTC なので +9 時間して -1 日
+            var date = DateTime.UtcNow.AddHours(9).AddDays(-1).ToString("yyyy-MM-dd");
+
+            // ****** はメモしたビューの ID
+            var analyticsViewId = ConfigurationManager.AppSettings["analyticsViewId"];
+            var data = await service.Data.Ga.Get("ga:" + analyticsViewId, date, date, "ga:pageviews").ExecuteAsync();
+
+            return int.Parse(data.Rows[0][0]);
+        }
+    }
+}
---
+++ WebAPI/Controllers/TweetPvController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using CoreTweet;
+
+namespace WebAPI.Controllers
+{
+    public class TweetPvController : ApiController
+    {
+        // GET api/values
+        public async Task<string> Get()
+        {
+            // アプリケーションの設定方法の詳細については、http://go.microsoft.com/fwlink/?LinkID=316888 を参照してください
+
+            var consumerKey = ConfigurationManager.AppSettings["consumerKey"];
+            var consumerSecret = ConfigurationManager.AppSettings["consumerSecret"];
+            var accessToken = ConfigurationManager.AppSettings["accessToken"];
+            var accessSecret = ConfigurationManager.AppSettings["accessSecret"];
+
+            var token = Tokens.Create(consumerKey,consumerSecret,accessToken,accessSecret);
+            var pv = await Analytics.GetPvAsync();
+
+            var message = string.Format("昨日のなか日記のPVは{0}でした", pv);
+            await token.Statuses.UpdateAsync(status => message);
+
+            return message;
+        }
+    }
+}
